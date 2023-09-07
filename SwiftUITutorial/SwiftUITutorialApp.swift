2f252db3440c9e9b5662959751f1c0d01a1d9af0
Justyn Jones <justynbjones@gmail.com>
2023-09-06 17:56:55 -0700

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Justyn Jones on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
