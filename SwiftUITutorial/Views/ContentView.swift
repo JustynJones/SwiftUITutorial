15b4eab7484c2a9642b10ded546564d156fb4d9d
Justyn Jones <justynbjones@gmail.com>
2023-09-17 00:05:02 -0700

    Completed the third section of the SwiftUI Tutorial, Handling User Input

---
+++ SwiftUITutorial/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Justyn Jones on 9/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
